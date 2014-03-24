09e0fe00741f96319c7bfa893c1d3e6661034bd6
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-24 12:03:49 +0100

    - updated the main wordpress plugin file to use the new code

--- trunk/factlink.php
+++ trunk/factlink.php
@@ -12,10 +12,34 @@ Version: 1.1
 Author URI: https://factlink.com/
 */
 
-defined( 'ABSPATH' ) or die ( 'Cannot access pages directly.' );
+// for protecting files from direct access
+defined('ABSPATH') or die ('Cannot access pages directly.');
 
-function add_factlink() {
-  echo '<script async="async" defer="defer" src="https://static.factlink.com/lib/dist/factlink_loader.min.js?o=wordpress"></script>';
+// load the wordpress plugin class
+include plugin_dir_path( __FILE__ ) . '/lib/wordpress_plugin.php';
+
+// declare the factlink class
+class Factlink extends \vg\wordpress_plugin\WordpressPlugin
+{
+    // set the namespace
+    public $namespace = 'factlink';
+
+    // add all the capabilities
+    protected function setup_capabilities()
+    {
+        // capability for displaying a notice when factlink isn't configured
+        $this->add_capability('configuration_notice', 'admin_notices');
+
+        // capability for adding a meta box to a blog or page edit page for enabling/disabling factlink
+        $this->add_capability('meta_box', 'add_meta_boxes');
+
+        // create the factlink settings page
+        $this->add_capability('admin_page', 'admin_menu');
+
+        // add the actual factlink javascript code to wordpress
+        $this->add_capability('include_factlink', 'wp_head');
+    }
 }
 
-add_action( 'wp_head', 'add_factlink' );
+// create a new factlink instance in the factlink namespace
+$factlink_plugin = new Factlink();
