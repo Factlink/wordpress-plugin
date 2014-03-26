ee435f8c68c492dafdea0fdf0848d44ebc3b8a72
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-26 17:27:53 +0100

    - fixed that the configuration notice isn't displayed when you go to the factlink settings page

--- trunk/app/capability/configuration_notice.php
+++ trunk/app/capability/configuration_notice.php
@@ -15,10 +15,19 @@ class ConfigurationNotice extends \vg\wordpress_plugin\capability\Capability
 
     public function initialize()
     {
-        if ($this->settings->is_configured->get() == 0)
+        // TODO: maybe use $_GET['page'] instead of current screen?
+
+        $menu_slug = 'factlink_settings_page';
+
+        $screen = get_current_screen();
+
+
+
+        // if not yet configured AND isn't on the settings page, display the notification
+        if ($this->settings->is_configured->get() == 0 && $screen->id != "settings_page_$menu_slug")
         {
             $parent_slug = 'options-general.php';
-            $menu_slug = 'factlink_settings_page';
+
             $this->url = get_admin_url(null, $parent_slug . "?page=" . $menu_slug);
             $this->render();
         }
