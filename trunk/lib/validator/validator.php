295726e07f9d25e743bca5d4943c793f0aaf8e08
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-26 15:38:12 +0100

    - created stub methods with exceptions for templating a validator

--- trunk/lib/validator/validator.php
+++ trunk/lib/validator/validator.php
@@ -4,5 +4,13 @@ namespace vg\wordpress_plugin\validator;
 
 class Validator
 {
+    public function error_message($value)
+    {
+        throw new \Exception("Validator: error_message method should be overridden.");
+    }
 
+    public function validate($value)
+    {
+        throw new \Exception("Validator: is_valid method should be overridden.");
+    }
 }
