7ff66868b7b0066bb484ae99d256be1fc9f01789
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-26 17:25:13 +0100

    - added option for storing preference of disabling the wordpress comments in the database

--- trunk/app/model/settings.php
+++ trunk/app/model/settings.php
@@ -13,6 +13,7 @@ class Settings extends \vg\wordpress_plugin\model\Model
     public $enabled_for_posts;
     public $enabled_for_pages;
     public $is_configured;
+    public $disable_global_comments;
 
     public $post_meta;
     public $page_meta;
@@ -29,8 +30,10 @@ class Settings extends \vg\wordpress_plugin\model\Model
         // setting to display configuration message as long factlink isn't configured
         $this->is_configured = $this->create_option_meta('is_configured', 'global_settings', 0, ['int']);
 
+        // settings for totally disabling global comments
+        $this->disable_global_comments = $this->create_option_meta('disable_global_comments', 'global_settings', 1, ['int']);
+
         // get a post meta data object
-        // TODO: post/page meta should also have validators instead of callback functions
         $this->post_meta = $this->create_post_meta('post', 'is_enabled', 1, ['int']);
 
         // create page meta object
