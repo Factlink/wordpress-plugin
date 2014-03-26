b507134f8fb70c755487ceaaa878cb92120fb4e3
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-26 17:23:55 +0100

    - added field for disabling the wordpress comment system

--- trunk/app/view/admin_page.php
+++ trunk/app/view/admin_page.php
@@ -15,18 +15,21 @@
             do_settings_sections( $this->option_group );
         ?>
 
-
-
-        <input type="hidden" name="<?php echo $this->settings->enabled_for_posts->name(true); ?>" value="0">
-        <input type="checkbox" name="<?php echo $this->settings->enabled_for_posts->name(true); ?>" <?php if ($this->settings->enabled_for_posts->get() == 1){ echo 'checked'; } ?> id="<?php echo $this->settings->enabled_for_posts->name(true); ?>" value="1">
-        <label for="<?php echo $this->settings->enabled_for_posts->name(true); ?>">Enabled for posts</label>
+        <input type="hidden" name="<?php echo $this->settings->enabled_for_posts->name(); ?>" value="0">
+        <input type="checkbox" name="<?php echo $this->settings->enabled_for_posts->name(); ?>" <?php if ($this->settings->enabled_for_posts->get() == 1){ echo 'checked'; } ?> id="<?php echo $this->settings->enabled_for_posts->name(); ?>" value="1">
+        <label for="<?php echo $this->settings->enabled_for_posts->name(); ?>">Enabled for posts</label>
 
         <br />
 
+        <input type="hidden" name="<?php echo $this->settings->enabled_for_pages->name(); ?>" value="0">
+        <input type="checkbox" name="<?php echo $this->settings->enabled_for_pages->name(); ?>" <?php if ($this->settings->enabled_for_pages->get() == 1){ echo 'checked'; } ?> id="<?php echo $this->settings->enabled_for_pages->name(); ?>" value="1">
+        <label for="<?php echo $this->settings->enabled_for_pages->name(); ?>">Enabled for pages</label>
 
-        <input type="hidden" name="<?php echo $this->settings->enabled_for_pages->name(true); ?>" value="0">
-        <input type="checkbox" name="<?php echo $this->settings->enabled_for_pages->name(true); ?>" <?php if ($this->settings->enabled_for_pages->get() == 1){ echo 'checked'; } ?> id="<?php echo $this->settings->enabled_for_pages->name(true); ?>" value="1">
-        <label for="<?php echo $this->settings->enabled_for_pages->name(true); ?>">Enabled for pages</label>
+        <br />
+
+        <input type="hidden" name="<?php echo $this->settings->disable_global_comments->name(); ?>" value="0">
+        <input type="checkbox" name="<?php echo $this->settings->disable_global_comments->name(); ?>" <?php if ($this->settings->disable_global_comments->get() == 1){ echo 'checked'; } ?> id="<?php echo $this->settings->disable_global_comments->name(); ?>" value="1">
+        <label for="<?php echo $this->settings->disable_global_comments->name(); ?>">Totally replace wordpress comments with Factlink</label>
 
         <?php submit_button(); ?>
 
