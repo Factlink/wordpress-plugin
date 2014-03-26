c93e7db78ec351f0fa43a38740e1fc277c2c16dc
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-26 15:37:37 +0100

    - implemented the int validator

--- trunk/lib/validator/int.php
+++ trunk/lib/validator/int.php
@@ -2,15 +2,20 @@
 
 namespace vg\wordpress_plugin\validator;
 
-class IntValidator extends Validator
+class Int extends Validator
 {
     public function error_message($value)
     {
         return "The value '$value' should be an int";
     }
 
-    public function is_valid($value)
+    public function validate($value)
     {
-        return false;
+        $ival = intval($value);
+
+
+
+        return (''.$ival === ''.$value);
+
     }
 }
