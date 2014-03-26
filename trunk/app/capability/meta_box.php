574edc7e1e570fd2a2dff58ebf724fb0dfb94944
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-26 15:45:45 +0100

    - Updated so the metabox now uses the model and meta classes

--- trunk/app/capability/meta_box.php
+++ trunk/app/capability/meta_box.php
@@ -4,19 +4,46 @@ namespace factlink\capability;
 
 class MetaBox extends \vg\wordpress_plugin\capability\Capability
 {
-    public function initialize()
+    /***
+     * inject
+     * @var \factlink\model\Settings
+     */
+    public $settings;
+
+    public $meta_name;
+    public $meta_value;
+
+    public function initialize($post_type, $post)
     {
+        $this->meta_name = $this->settings->post_meta->name;
+        $this->meta_value = $this->settings->post_meta->get($post->ID);
+
         $id = 'factlink-settings-meta';
         $title = 'FactLink settings';
-        $callback = array($this, 'render');
-        $post_type = 'post';
         $context = 'advanced';
 
-        add_meta_box($id, $title, $callback, $post_type, $context);
+        if ($this->settings->enabled_for_pages->get() == 1 && $post_type == 'page')
+        {
+            add_meta_box($id, $title, array($this, 'render_page_meta_box'), 'page', $context);
+        }
+
+        if ($this->settings->enabled_for_posts->get() == 1 && $post_type == 'post')
+        {
+            add_meta_box($id, $title, array($this, 'render_post_meta_box'), 'post', $context);
+        }
     }
 
-    public function render()
+    public function render_page_meta_box($post_data)
     {
-        echo "Checkbox for enabling or disabling factlink for this post";
+        $this->meta_name = $this->settings->page_meta->name;
+        $this->render();
     }
+
+    public function render_post_meta_box($post_data)
+    {
+        $this->meta_name = $this->settings->post_meta->name;
+        $this->render();
+    }
+
+
 }
