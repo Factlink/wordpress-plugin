1ea644eb5313a316dda999cecc4dbcd10778fd00
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-26 15:47:08 +0100

    - Injected the settings model

--- trunk/app/capability/configuration_notice.php
+++ trunk/app/capability/configuration_notice.php
@@ -7,12 +7,20 @@ class ConfigurationNotice extends \vg\wordpress_plugin\capability\Capability
     // public members are available in the view
     public $url;
 
+    /***
+     * inject:
+     * @var \factlink\model\Settings
+     */
+    public $settings;
+
     public function initialize()
     {
-        $parent_slug = 'options-general.php';
-        $menu_slug = 'factlink_settings_page';
-        $this->url = get_admin_url(null, $parent_slug . "?page=" . $menu_slug);
-
-        $this->render();
+        if ($this->settings->is_configured->get() == 0)
+        {
+            $parent_slug = 'options-general.php';
+            $menu_slug = 'factlink_settings_page';
+            $this->url = get_admin_url(null, $parent_slug . "?page=" . $menu_slug);
+            $this->render();
+        }
     }
 }
