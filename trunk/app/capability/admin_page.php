733475f5fe0794d002e7a6afc96d82d02ee6e198
Maarten van Gijssel <maarten@vgijssel.nl>
2014-03-26 15:48:47 +0100

    - Added calls to register the displayed options otherwise Wordpress won't save the data and disable the configuration notice when visiting the admin page`

--- trunk/app/capability/admin_page.php
+++ trunk/app/capability/admin_page.php
@@ -9,8 +9,6 @@ class AdminPage extends \vg\wordpress_plugin\capability\Capability
      */
     public $settings;
 
-    private $settings_group = 'factlink_settings_group';
-
     public function initialize()
     {
         // settings for the admin page
@@ -21,10 +19,26 @@ class AdminPage extends \vg\wordpress_plugin\capability\Capability
         $menu_slug = 'factlink_settings_page';
 
         // use the builtin render
-        $render_callback = array($this, 'render');
+        $render_callback = array($this, 'admin_page_requested');
 
         // create a sub-level menu
         add_submenu_page($parent_slug, $page_title, $menu_title, $capability, $menu_slug, $render_callback);
 
+        // register the options that can be updated through the view
+        $this->settings->enabled_for_pages->register();
+        $this->settings->enabled_for_posts->register();
+    }
+
+    public function admin_page_requested()
+    {
+        // set the used option group
+        $this->option_group = $this->settings->enabled_for_posts->group;
+
+        // the configuration notice will be disabled
+        // TODO: should be placed somewhere else, because now still displays one time when configuration is loaded
+        $this->settings->is_configured->set(1);
+
+        // render the admin page
+        $this->render();
     }
 }
