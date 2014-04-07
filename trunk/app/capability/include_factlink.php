8362494c7f014a212fc0bd3d3dd8f9d343aab545
maarten <maarten@vgijssel.nl>
2014-04-07 15:20:18 +0200

    Moved 'is Factlink enabled' logic to the settings model.

--- trunk/app/capability/include_factlink.php
+++ trunk/app/capability/include_factlink.php
@@ -14,27 +14,8 @@ class IncludeFactlink extends \vg\wordpress_plugin\capability\Capability
     {
         $id = get_queried_object_id();
 
-        if ($this->settings->enabled_for_pages->get() == 1 &&
-
-            is_page() &&
-
-            ($this->settings->enabled_for_all_pages->get() == 0 && $this->settings->page_meta->get($id) == 1) ||
-
-            $this->settings->enabled_for_all_pages->get() == 1
-
-        ) {
-            $this->render();
-        }
-
-        if ($this->settings->enabled_for_posts->get() == 1 &&
-
-            is_single() &&
-
-            ($this->settings->enabled_for_all_posts->get() == 0 && $this->settings->post_meta->get($id) == 1) ||
-
-            $this->settings->enabled_for_all_posts->get() == 1
-        ) {
-            // render factlink
+        if ($this->settings->is_enabled_for_post($id))
+        {
             $this->render();
         }
     }
