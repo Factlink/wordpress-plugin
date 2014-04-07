83177ba9c5aa84963e7218d4d7d73f3dfa972dd7
maarten <maarten@vgijssel.nl>
2014-04-07 15:57:08 +0200

    Changed comparison to builtin negation.

--- trunk/app/view/meta_box.php
+++ trunk/app/view/meta_box.php
@@ -1,6 +1,6 @@
 <input type="hidden" name="<?php echo $this->meta_name; ?>" value="0">
 <input type="checkbox" name="<?php echo $this->meta_name; ?>" value="1"
-       id="<?php echo $this->meta_name; ?>" <?php if ($this->meta_value == 1 || $this->is_published == false) {
+       id="<?php echo $this->meta_name; ?>" <?php if ($this->meta_value == 1 || !$this->is_published) {
     echo 'checked';
 } ?>>
 <label for="<?php echo $this->meta_name; ?>">Enable Factlink for this item</label>
