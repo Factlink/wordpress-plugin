facfcefa40e3c4ac7a35bfda0e3875d20537560a
maarten <maarten@vgijssel.nl>
2014-04-15 17:02:35 +0200

    Fix adds check for post type to prevent php notices.

--- trunk/app/capability/meta_box.php
+++ trunk/app/capability/meta_box.php
@@ -17,25 +17,25 @@ class MetaBox extends \vg\wordpress_plugin\capability\Capability
 
     public function initialize($post_type, $post)
     {
-        $is_enabled_option = $this->settings->is_enabled_options[$post_type];
-        $post_meta = $this->settings->meta[$post_type];
-
-        // get the post status of the current post
-        //$this->is_published = get_post_status(get_queried_object_id()) == 'publish';
-
-        $this->is_published = $post->post_status == 'publish';
-
-        if ($is_enabled_option->get() == 1)
+        if (isset($this->settings->is_enabled_options[$post_type]))
         {
-            $id = 'factlink-settings-meta';
-            $title = 'FactLink settings';
-            $context = 'advanced';
+            $is_enabled_option = $this->settings->is_enabled_options[$post_type];
+            $post_meta = $this->settings->meta[$post_type];
 
-            $this->meta_name = $post_meta->name();
-            $this->meta_value = $post_meta->get($post->ID);
+            $this->is_published = $post->post_status == 'publish';
 
-            // can't directly call the render callback, because the render callback passes arguments
-            add_meta_box($id, $title, array($this, 'render_meta_box'), $post_type, $context);
+            if ($is_enabled_option->get() == 1)
+            {
+                $id = 'factlink-settings-meta';
+                $title = 'FactLink settings';
+                $context = 'advanced';
+
+                $this->meta_name = $post_meta->name();
+                $this->meta_value = $post_meta->get($post->ID);
+
+                // can't directly call the render callback, because the render callback passes arguments
+                add_meta_box($id, $title, array($this, 'render_meta_box'), $post_type, $context);
+            }
         }
     }
 
